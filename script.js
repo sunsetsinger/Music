be18f45e0edfe64720b87562f78acb81a780c03b
sunsetsinger <sunsetsinger@gmail.com>
2024-11-07 23:48:34 -0500

    play buttons

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+
   const playButton = document.getElementById("playButton");
   const audio = document.getElementById("myAudio");
+  let is_playing = false;
+
 
   function playAudio() {
-    audio
-      .play()
-      .then(() => {
-        console.log("Audio is playing");
-      })
-      .catch((error) => {
-        console.error("Error playing audio:", error);
-      });
+
+    if (!is_playing) {
+      is_playing = true;
+      audio
+        .play()
+        .then(() => {
+          console.log("Audio is playing");
+        })
+        .catch((error) => {
+          console.error("Error playing audio:", error);
+        });
+
+
+        playButton.src = "Assets/svg/pause_button.svg"
+
+    } else {
+      is_playing = false
+      console.log("audio stop")
+      audio.pause();
+
+      playButton.src = "Assets/svg/playbutton.svg"
+
+    }
   }
 
   playButton.addEventListener("click", playAudio);
